f6d051810b50b19cfbaec6eb9385851a9f738719
Ronald <rdominguez2475@gmail.com>
2022-05-01 07:11:07 -0300

    modelos

---
+++ voluntariado/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Facilitador(models.Model):
+    nombre=models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
+    email=models.EmailField()
+    presentacion=models.TextField()
+
+class Voluntario(models.Model):
+    nombre=models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
+    email=models.EmailField()
