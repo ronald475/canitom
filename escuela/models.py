2eb059e721af4953d858c4eee0ea1c05ab731ccd
Ronald <rdominguez2475@gmail.com>
2022-05-01 07:19:57 -0300

    migracion 2

--- escuela/models.py
+++ escuela/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Facilitador(models.Model):
+class Clase(models.Model):
     titulo = models.CharField(max_length=40)
     tema = models.CharField(max_length=40)
     contenido = models.TextField()
