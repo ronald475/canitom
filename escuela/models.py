36fa66946fbdbd34413644bff2d52b354716b902
Ronald <rdominguez2475@gmail.com>
2022-05-01 07:16:10 -0300

    migracion

--- escuela/models.py
+++ escuela/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Facilitador(models.Model):
-    titulo=models.CharField(max_length=40)
-    tema=models.CharField(max_length=40)
-    contenido=models.TextField()
-    fecha=models.DateField()
+    titulo = models.CharField(max_length=40)
+    tema = models.CharField(max_length=40)
+    contenido = models.TextField()
+    fecha = models.DateField()
